dbec8d11f764cbe47379b3c5012974ca256a556a
MJanowskiDev <mat.krz.janowski@gmail.com>
2021-12-02 21:36:16 +0100

    Renamed utils folder to services

--- src/containers/UsersContainer/UsersContainer.js
+++ src/containers/UsersContainer/UsersContainer.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { UserList, Loading } from '../../components';
-import fetchUsers from '../../utils/fetchUsers';
+import fetchUsers from '../../services/fetchUsers';
 
 const styles = {
 	container: {
